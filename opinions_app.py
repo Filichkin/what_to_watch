765918257739357eb9acb591a43bf3711cdb10d4
Alexey Filichkin <filichkin.alexey@gmail.com>
2025-07-18 18:23:28 +0300

    upd index_view

--- opinions_app.py
+++ opinions_app.py
@@ -1,3 +1,6 @@
+from datetime import datetime, timezone
+from random import randrange
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
@@ -9,10 +12,37 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 db = SQLAlchemy(app)
 
 
+class Opinion(db.Model):
+    id = db.Column(
+        db.Integer,
+        primary_key=True
+        )
+    title = db.Column(
+        db.String(128),
+        nullable=False
+        )
+    text = db.Column(
+        db.Text,
+        unique=True,
+        nullable=False
+        )
+    source = db.Column(db.String(256))
+    timestamp = db.Column(
+        db.DateTime,
+        index=True,
+        default=datetime.now(timezone.utc)
+        )
+
+
 @app.route('/')
 def index_view():
+    quantity = Opinion.query.count()
+    if not quantity:
+        return 'В базе данных мнений о фильмах нет.'
 
-    return 'Совсем скоро тут будет случайное мнение о фильме!'
+    offset_value = randrange(quantity)
+    opinion = Opinion.query.offset(offset_value).first()
+    return opinion.text
 
 
 if __name__ == '__main__':
